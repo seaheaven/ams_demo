09136fb22820b0a1069ad0190a7e258973ec7282
yirun <15109175+yirunkong@user.noreply.gitee.com>
2024-12-27 07:18:14 +0800

    把发布libTest生成的jar应用到项目插桩

--- app/src/main/java/com/yirun/amsplugindemo/MainActivity.kt
+++ app/src/main/java/com/yirun/amsplugindemo/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.yirun.amsplugindemo.ui.theme.AmsPluginDemoTheme
+import com.yirun.libtest.Test
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
