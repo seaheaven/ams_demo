e6c1777d92001c6c5a650967fb379793b90b573b
yirun <seaheavenbnn@163.com>
2025-01-12 12:26:30 +0800

    mod 移除方法实现

--- libAsmPlugin/src/main/java/com/yirun/asmplugin/TestClassVisitor.kt
+++ libAsmPlugin/src/main/java/com/yirun/asmplugin/TestClassVisitor.kt
@@ -6,6 +6,8 @@ import org.objectweb.asm.Opcodes
 
 class TestClassVisitor(classVisitor: ClassVisitor): ClassVisitor(Opcodes.ASM7, classVisitor) {
     private var className: String? = null
+    private val mMethodName = "getDeviceId"
+    private val mMethodDesc = "(Landroid/content/Context;)Ljava/lang/String;"
     override fun visit(
         version: Int,
         access: Int,
@@ -23,11 +25,17 @@ class TestClassVisitor(classVisitor: ClassVisitor): ClassVisitor(Opcodes.ASM7, c
         descriptor: String?,
         signature: String?,
         exceptions: Array<out String>?
-    ): MethodVisitor {
-        val methodVisitor = super.visitMethod(access, name, descriptor, signature, exceptions)
+    ): MethodVisitor? {
+
         if(className == "com/otherlib/util/DeviceUtil"){
+            if(name == mMethodName && (descriptor == mMethodDesc)){
+                println("---------移除 getDeviceId 方法------------")
+                return null
+            }
+            val methodVisitor = super.visitMethod(access, name, descriptor, signature, exceptions)
             return ReplaceMethodVisitor(api, methodVisitor, access, name?:"", descriptor?:"", className?:"")
         }else {
+            val methodVisitor = super.visitMethod(access, name, descriptor, signature, exceptions)
             return CostTimeMethodVisitor(api, methodVisitor, access, name?:"", descriptor?:"", className?:"")
         }
     }
